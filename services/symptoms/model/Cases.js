a4bcfc7830d7e819dd3eb8ebf5f791c0ad84da95
WEAVES\Tarun <tarunjr@gmail.com>
2016-09-10 21:35:58 +0530

    Case mongoose model

--- services/symptoms/model/Cases.js
+++ services/symptoms/model/Cases.js
@@ -3,6 +3,51 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var caseSchema = new Schema({
-  	id:  String,
+  	patient:{
+  		id: String,
+  		name: String
+  	},
+  	doctor: {
+  		id: String,
+  		name: String,
+  	},
+  	provider: {
+  		id: String,
+  		name: String,	
+  	},
+  	status: String,
+  	owner: String,
+  	data: {
+  		clinical: [{
+  			id: String,
+  			name: String,	
+  			value: String
+  		}],
+  		test: [{
+  			id: String,
+  			name: String,	
+  			value: String
+  		}]
+  	},
+  	pendingdata: {
+  		clinical: [{
+  			id: String,
+  			name: String
+  		}],
+  		test: [{
+  			id: String,
+  			name: String	
+  		}]
+  	},
+  	prescription: {
+  		date:String,
+  		medicines: [ {
+  			name: String,
+  			intake: String,
+  			duration: String
+  		}],
+  		notes: String,
+  		review: String
+  	}
   });
 module.exports = mongoose.model('Cases', caseSchema);
