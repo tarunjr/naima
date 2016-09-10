7b5354e40e1f0e67951ac9e4a0d9f7c06c856c8d
Saurabh Minni <saurabh@adnear.com>
2016-09-11 04:13:19 +0530

    Added code for doctor matching for symptoms

--- services/symptoms/controller/cases.js
+++ services/symptoms/controller/cases.js
@@ -91,12 +91,66 @@ exports.put_case_data = function(req, res) {
 exports.put_new_case_data = function(req, res) {
 	data = req.body;
 	case_data = { status : "open", owner :"doctor", data : data.data, provider: data.provider };
-	var Cases = mongoose.model('Cases');
-	Cases.create(case_data, function(err, data){
-		if(err)
-			res.send("failed");
-		else {
-			res.send("success");
+
+	var Conditions = mongoose.model('Conditions');
+	symptomIds = [];
+	for(iCount = 0; iCount < data.data.clinical.length; iCount++){
+		symptomIds.push(data.data.clinical[iCount].id);
+	}
+	console.log(symptomIds);
+	Conditions.find({ 'data.clinical.id' : {$in: symptomIds}})
+	.exec( function (err, data){
+
+	})
+	.then(function (data){
+		map = new Map();
+		for(iCount = 0; iCount < data.length; iCount++){
+			map.get(data[iCount].speciality.id) == undefined ? map.set(data[iCount].speciality.id, 1) : map.set(data[iCount].speciality.id, map.get(data[iCount].speciality.id) + 1);
 		}
+		console.log(map);
+		maxid = "";
+		maxval = 0;
+		map.forEach( function(value, key){
+			if(maxval < value ){
+				maxval = value;
+				maxid = key;
+			}
+		}, map);
+		var Doctors  =  mongoose.model('Doctors');
+		// res.send("success");
+		Doctors.find({"speciality.id" : maxid }, function(err, data){
+			if (err)
+			  res.send("failed");
+			else{
+				console.log(data);
+				var Cases = mongoose.model('Cases');
+				case_data.doctor = {};
+				case_data.doctor['id'] = data[0].id;
+				case_data.doctor['name'] = data[0].name;
+				console.log(case_data);
+				Cases.create(case_data, function(err, data){
+					if(err)
+						res.send("failed");
+					else {
+						res.send("success");
+					}
+				});
+
+			}
+		});
+
 	});
+	// Conditions.find({'data.clinical.id' : data.data.clinical[iCount].id, 'data.clinical.value' : data.data.clinical[iCount].value}, function(err, data){
+	//
+	// 	}
+	// });
+
+	// var Cases = mongoose.model('Cases');
+	// Cases.create(case_data, function(err, data){
+	// 	if(err)
+	// 		res.send("failed");
+	// 	else {
+	// 		res.send("success");
+	// 	}
+	// });
 }
