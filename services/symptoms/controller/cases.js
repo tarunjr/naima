e0dec2ba0dbc08a1af5dfb9405378f1be69f77fc
WEAVES\Tarun <tarunjr@gmail.com>
2016-09-11 10:51:25 +0530

    Bug in patient

--- services/symptoms/controller/cases.js
+++ services/symptoms/controller/cases.js
@@ -91,7 +91,7 @@ exports.put_case_data = function(req, res) {
 exports.put_new_case_data = function(req, res) {
 	data = req.body;
 	console.log(data);
-	case_data = { status : "open", owner :"doctor", data : data.data, provider: data.provider };
+	case_data = { status : "open", owner :"doctor", data : data.data, provider: data.provider, patient: data.patient };
 
 	var Conditions = mongoose.model('Conditions');
 	symptomIds = [];
