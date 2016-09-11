ebbf85d6cadd1652a14e7791552e29f9d17d3917
WEAVES\Tarun <tarunjr@gmail.com>
2016-09-11 13:48:25 +0530

    Updates related to DB

--- services/symptoms/db_cases_symptoms.js
+++ services/symptoms/db_cases_symptoms.js
@@ -2,8 +2,6 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 var db = mongoose.connection;
 
-
-
 db.on('error', console.error);
 db.once('open', function() {
   var Symptoms = require('./model/Symptoms.js');
@@ -13,13 +11,12 @@ db.once('open', function() {
   var Doctors = require('./model/Doctors.js');
   var Rules = require('./model/AssociationRules.js');
 
-  Conditions.find({}, function(err, condition) {
+  Cases.find({}, function(err, cases) {
     if (err)
       console.log(JSON.stringify({}));
     else {
-      var data = exportSymtomsList(condition);
-      
-      fs.writeFile("db_symptoms.json", data, function(err) {
+      var data = exportCaseSymtomsList(cases);
+      fs.writeFile("db_case_symptoms.json", data, function(err) {
       if(err) {
           return console.log(err);
       }
@@ -32,19 +29,6 @@ db.once('open', function() {
 
 mongoose.connect('mongodb://localhost/test');
 
-function exportSymtomsList (conditions) {
-  var all = []
-  for (i = 0; i < conditions.length; i++) { 
-      var symptomIds = []
-      //symptomIds.push(conditions[i].id);
-      var clinicalList = conditions[i].data.clinical;
-      for (j = 0; j < clinicalList.length; j++) { 
-           symptomIds.push(clinicalList[j].id);
-      } 
-      all.push(symptomIds.join())
-  }
-  return all.join('\n');
-}
 function exportCaseSymtomsList (cases) {
   var all = []
   for (i = 0; i < cases.length; i++) { 
